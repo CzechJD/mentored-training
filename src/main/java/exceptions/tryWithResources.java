66689e71a2d727541fe09b11362ce1faa3dc78fc
CzechJD <stas.kosach848@gmail.com>
2023-12-22 16:28:30 +0300

    Задание с исключениями

---
+++ src/main/java/exceptions/tryWithResources.java
@@ -0,0 +1,41 @@
+package exceptions;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class tryWithResources {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String pathToFile = scanner.nextLine();
+        readFile("C:\\Users\\CzechB4\\OneDrive\\Рабочий стол\\source.txt");
+        writeInFile("C:\\Users\\CzechB4\\OneDrive\\Рабочий стол\\copyFile.txt");
+    }
+
+    public static String readFile(String pathToFile) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(pathToFile))) {
+            String result;
+            while ((result = bufferedReader.readLine()) != null) {
+                stringBuilder.append(result).append("\n");
+            }
+        } catch (FileNotFoundException exception) {
+            throw new RuntimeException("Не удалось найти файл");
+        } catch (IOException exception) {
+            throw new RuntimeException("Нам капец. Конец связи.");
+        }
+        return String.valueOf(stringBuilder);
+    }
+
+    public static void writeInFile(String createFile) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(createFile))) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Укажите путь к файлу, из которого нужно копировать данные: ");
+            String copyFromFile = scanner.nextLine();
+            bufferedWriter.write(readFile(copyFromFile));
+        } catch (FileNotFoundException exception) {
+            throw new RuntimeException("Указанный файл не найден :(");
+        } catch (IOException exception) {
+            throw new RuntimeException("Хьюстон, у нас проблемы...");
+        }
+    }
+}
