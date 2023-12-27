bf77b75f57d61eaf9d7b572e6dd2564e1876df21
CzechJD <stas.kosach848@gmail.com>
2023-12-27 14:21:08 +0300

    1. Классы стали record, что позволяет сократить код, не потеряв функциональности 2. Добавлена структура данных HashMap<Long, Boolean>, чтобы определить, свободна книга или нет. 3. Исправлены методы: - getAllBooks() - getUserBooks() - takeBook() - returnBook() в связи с добавлением новой структуры данных 4. Все методы исправлены согласно комментариям 5. Теперь пользователи и книги добавляются в классе Main отдельно к каждому объекту, а не создаются вместе с объектом, как было раньше. 6. Добавлен методы addBook() и addUser() для добавления книг и пользователей к определённому объекту 7. Исправлены тесты: теперь для каждого теста создаётся отдельный объект Библиотеки. 8. Добавлены геттеры к классам: User, Book, LibraryService

--- src/main/java/com/example/collections/LibraryService.java
+++ src/main/java/com/example/collections/LibraryService.java
@@ -2,91 +2,60 @@ package com.example.collections;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class LibraryService {
-    private HashMap<Long, Book> books;
-    private HashMap<Long, User> users;
-    private HashMap<Long, ArrayList<Long>> userBorrowedBooks = new HashMap<>();
+    private final HashMap<Long, Book> books; // Список всех книг по id
+    private final HashMap<Long, User> users; // Список всех пользователей по id
+    private final HashMap<Long, Boolean> isBookTaken; // список свободных книг
+    private final HashMap<Long, ArrayList<Book>> userBorrowedBooks;
 
     public LibraryService() {
-        this.books = new HashMap<>();
-        this.users = new HashMap<>();
-        this.userBorrowedBooks = new HashMap<>();
-
-        this.books.put(1L, new Book("The Infernal Devices", "Kassandra Clare", 2010, 1L));
-        this.books.put(2L, new Book("Часовой ключ", "Наталья Щерба", 2011, 2L));
-        this.books.put(3L, new Book("Зерцалия", "Евгений Гаглоев", 2018, 3L));
-        this.books.put(4L, new Book("Mara and Morok", "Leah Arden", 2020, 4L));
-        this.books.put(5L, new Book("The Subtle Art of Not Giving a F*ck", "Mark Manson", 2016, 5L));
-
-        this.users.put(1L, new User("William", 17, 1L));
-        this.users.put(2L, new User("Charlotte", 23, 2L));
-        this.users.put(3L, new User("James", 17, 3L));
-        this.users.put(4L, new User("Henry", 23, 4L));
-        this.users.put(5L, new User("Tessa", 16, 5L));
+        books = new HashMap<>();
+        users = new HashMap<>();
+        isBookTaken = new HashMap<>();
+        userBorrowedBooks = new HashMap<>();
 
     }
 
-    public static void main(String[] args) {
-        LibraryService libraryService = new LibraryService();
-    }
 
     public ArrayList<Book> getAllBooks() {
         return new ArrayList<>(books.values());
     }
 
     public ArrayList<Book> getAllAvailableBooks() {
-        ArrayList<Book> availableBook = new ArrayList<>();
-        for (Book book : books.values()) {
-            boolean isBookTaken = false;
-            for (ArrayList<Long> takenBook : userBorrowedBooks.values()) {
-                if (takenBook.contains(book.getBookId())) {
-                    isBookTaken = true;
-                    break;
-                }
-            }
-            if (!isBookTaken) {
-                availableBook.add(book);
+        ArrayList<Book> availableBooks = new ArrayList<>(); // создаём список доступных книг
+        for (Map.Entry<Long, Book> bookEntry : books.entrySet()) {
+            if (!isBookTaken.getOrDefault(bookEntry.getKey(), false)) {
+                isBookTaken.put(bookEntry.getKey(), false);
+                availableBooks.add(bookEntry.getValue());
             }
         }
-
-        return availableBook;
+        return availableBooks;
     }
 
     public ArrayList<Book> getUserBooks(Long userId) {
         if (!users.containsKey(userId)) {
             throw new IllegalArgumentException("Пользователь с ID: " + userId + ", не найден");
         }
-        ArrayList<Book> listBook = new ArrayList<>();
-
-        if (userBorrowedBooks.containsKey(userId)) {
-            ArrayList<Long> userBook = userBorrowedBooks.get(userId);
-            for (Long book : userBook) {
-                listBook.add(books.get(book));
-            }
-        }
-        return listBook;
+        return userBorrowedBooks.getOrDefault(userId, new ArrayList<>());
     }
 
     public boolean takeBook(Long userId, Long bookId) {
         if (!users.containsKey(userId)) {
             throw new IllegalArgumentException("Пользователь с ID: " + userId + ", не найден");
         }
-
         if (!books.containsKey(bookId)) {
             throw new IllegalArgumentException("Книга с ID: " + bookId + ", не найдена");
         }
 
-        for (ArrayList<Long> userBooks : userBorrowedBooks.values()) {
-            if (userBooks.contains(bookId)) {
-                return false;
-            }
+        if (isBookTaken.getOrDefault(bookId, false)) {
+            return false;
         }
-
-        userBorrowedBooks.computeIfAbsent(userId, b -> new ArrayList<>()).add(bookId);
+        userBorrowedBooks.computeIfAbsent(userId, v -> new ArrayList<>()).add(books.get(bookId));
+        isBookTaken.put(bookId, true);
         return true;
     }
-
 
     public Boolean returnBook(Long userId, Long bookId) {
         if (!users.containsKey(userId)) {
@@ -94,16 +63,39 @@ public class LibraryService {
         } else if (!books.containsKey(bookId)) {
             throw new IllegalArgumentException("Книга с ID: " + bookId + ", не найдена");
         }
-        if (userBorrowedBooks.containsKey(userId)) {
-            ArrayList<Long> userBooks = userBorrowedBooks.get(userId);
-            if (userBooks.contains(bookId)) {
-                userBooks.remove(bookId);
-                if (userBooks.isEmpty()) {
-                    userBorrowedBooks.remove(userId);
-                }
-                return true;
-            }
+
+        ArrayList<Book> borrowedBooks = userBorrowedBooks.get(userId);
+        if (borrowedBooks == null || !borrowedBooks.contains(books.get(bookId))) {
+            return false;
         }
-        return false;
+
+        borrowedBooks.remove(books.get(bookId));
+        isBookTaken.put(bookId, false);
+        return true;
+    }
+
+
+    public void addBook(Long id, Book book) {
+        books.put(id, book);
+    }
+
+    public void addUser(Long id, User user) {
+        users.put(id, user);
+    }
+
+    public HashMap<Long, Book> getBooks() {
+        return books;
+    }
+
+    public HashMap<Long, User> getUsers() {
+        return users;
+    }
+
+    public HashMap<Long, Boolean> getIsBookTaken() {
+        return isBookTaken;
+    }
+
+    public HashMap<Long, ArrayList<Book>> getUserBorrowedBooks() {
+        return userBorrowedBooks;
     }
 }
--- src/test/java/test/collections/testCollections.java
+++ src/test/java/test/collections/testCollections.java
@@ -2,6 +2,9 @@ package test.collections;
 
 import com.example.collections.Book;
 import com.example.collections.LibraryService;
+import com.example.collections.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,29 +14,48 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class testCollections {
+    private static LibraryService libraryServiceTest;
+    @BeforeEach
+    void setUp() {
+        libraryServiceTest = new LibraryService();
+
+        libraryServiceTest.addBook(1L, new Book("The Infernal Devices", "Kassandra Clare", 2010, 1L));
+        libraryServiceTest.addBook(2L, new Book("Часовой ключ", "Наталья Щерба", 2011, 2L));
+        libraryServiceTest.addBook(3L, new Book("Зерцалия", "Евгений Гаглоев", 2018, 3L));
+        libraryServiceTest.addBook(4L, new Book("Mara and Morok", "Leah Arden", 2020, 4L));
+        libraryServiceTest.addBook(5L, new Book("The Subtle Art of Not Giving a F*ck", "Mark Manson", 2016, 5L));
+
+        libraryServiceTest.addUser(1L, new User("William", 17, 1L));
+        libraryServiceTest.addUser(2L, new User("Charlotte", 23, 2L));
+        libraryServiceTest.addUser(3L, new User("James", 17, 3L));
+        libraryServiceTest.addUser(4L, new User("Henry", 23, 4L));
+        libraryServiceTest.addUser(5L, new User("Tessa", 16, 5L));
+    }
+
+
+
     @Test
-    public void getUserBooks() {
-        LibraryService libraryService = new LibraryService();
-        libraryService.takeBook(1L, 3L);
-        libraryService.takeBook(1L, 4L);
+    void getUserBooks() {
+        libraryServiceTest.takeBook(1L, 3L);
+        libraryServiceTest.takeBook(1L, 4L);
+
         List<Book> expectedBooks = Arrays.asList(
                 new Book("Зерцалия", "Евгений Гаглоев", 2018, 3L),
                 new Book("Mara and Morok", "Leah Arden", 2020, 4L));
-        List<Book> actualBooks = libraryService.getUserBooks(1L);
+        List<Book> actualBooks = libraryServiceTest.getUserBooks(1L);
 
         assertEquals(expectedBooks, actualBooks);
-        assertEquals(new ArrayList<>(), libraryService.getUserBooks(2L));
+        assertEquals(new ArrayList<>(), libraryServiceTest.getUserBooks(2L));
 
         assertThrows(IllegalArgumentException.class, () -> new LibraryService().getUserBooks(0L));
 
     }
 
     @Test
-    public void takeBook() {
-        LibraryService libraryService = new LibraryService();
-        assertEquals(true, libraryService.takeBook(1L, 1L));
-        assertEquals(true, libraryService.takeBook(2L, 3L));
-        assertEquals(false, libraryService.takeBook(2L, 1L));
+    void takeBook() {
+        assertTrue(libraryServiceTest.takeBook(1L, 1L));
+        assertTrue(libraryServiceTest.takeBook(2L, 3L));
+        assertFalse(libraryServiceTest.takeBook(2L, 1L));
 
         assertThrows(IllegalArgumentException.class, () -> new LibraryService().takeBook(0L, 1L));
         assertThrows(IllegalArgumentException.class, () -> new LibraryService().takeBook(0L, 0L));
@@ -41,11 +63,11 @@ public class testCollections {
     }
 
     @Test
-    public void returnBook() {
-        LibraryService libraryService = new LibraryService();
-        libraryService.takeBook(1L, 2L);
-        assertEquals(true, libraryService.returnBook(1L, 2L));
-        assertEquals(false, libraryService.returnBook(1L, 3L));
+    void returnBook() {
+        libraryServiceTest.takeBook(1L, 2L);
+        assertEquals(true, libraryServiceTest.returnBook(1L, 2L));
+        assertEquals(false, libraryServiceTest.returnBook(1L, 3L));
+
         assertThrows(IllegalArgumentException.class, () -> new LibraryService().returnBook(1L, 0L));
         assertThrows(IllegalArgumentException.class, () -> new LibraryService().returnBook(0L, 0L));
         assertThrows(IllegalArgumentException.class, () -> new LibraryService().returnBook(0L, 1L));
