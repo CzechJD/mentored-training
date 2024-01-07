bf4608f184a8736459a87b66fcfd65630ab79c8d
CzechJD <stas.kosach848@gmail.com>
2024-01-07 13:04:44 +0300

    Первые 5 задач StreamAPI (Увеличил количество заказов до 25)

--- src/main/java/com/example/streamapi/Main.java
+++ src/main/java/com/example/streamapi/Main.java
@@ -30,26 +30,41 @@ public class Main {
 
         Order order1 = new Order(1L, LocalDate.of(2021, 1, 5), LocalDate.of(2021, 1, 7), "reserved", orderBooks);
         Order order2 = new Order(2L, LocalDate.of(2021, 1, 10), LocalDate.of(2021, 1, 12), "on delivery", orderChildrenProduct);
-        Order order3 = new Order(3L, LocalDate.of(2021, 2, 15), LocalDate.of(2021, 2, 15), "delivered", orderToys);
-        Order order4 = new Order(4L, LocalDate.of(2021, 2, 20), LocalDate.of(2021, 2, 21), "reserved", orderSundry1);
-        Order order5 = new Order(5L, LocalDate.of(2021, 3, 9), LocalDate.of(2021, 3, 11), "delivered", orderSundry2);
-        Order order6 = new Order(6L, LocalDate.of(2021, 3, 24), LocalDate.of(2021, 3, 25), "reserved", orderSundry3);
-        Order order7 = new Order(7L, LocalDate.of(2021, 4, 13), LocalDate.of(2021, 4, 13), "delivered", orderToys);
-        Order order8 = new Order(8L, LocalDate.of(2021, 4, 29), LocalDate.of(2021, 4, 30), "on delivered", orderSundry2);
-        Order order9 = new Order(9L, LocalDate.of(2021, 5, 1), LocalDate.of(2021, 5, 2), "delivered", orderSundry1);
-        Order order10 = new Order(10L, LocalDate.of(2021, 5, 11), LocalDate.of(2021, 5, 15), "on delivered", orderBooks);
+        Order order3 = new Order(3L, LocalDate.of(2021, 1, 15), LocalDate.of(2021, 1, 15), "delivered", orderToys);
+        Order order4 = new Order(4L, LocalDate.of(2021, 1, 20), LocalDate.of(2021, 1, 21), "reserved", orderSundry1);
+        Order order5 = new Order(5L, LocalDate.of(2021, 1, 25), LocalDate.of(2021, 1, 26), "delivered", orderSundry2);
+        Order order6 = new Order(6L, LocalDate.of(2021, 2, 5), LocalDate.of(2021, 2, 7), "reserved", orderSundry3);
+        Order order7 = new Order(7L, LocalDate.of(2021, 2, 10), LocalDate.of(2021, 2, 12), "delivered", orderToys);
+        Order order8 = new Order(8L, LocalDate.of(2021, 2, 15), LocalDate.of(2021, 2, 17), "on delivered", orderSundry2);
+        Order order9 = new Order(9L, LocalDate.of(2021, 2, 25), LocalDate.of(2021, 2, 27), "delivered", orderSundry1);
+        Order order10 = new Order(10L, LocalDate.of(2021, 3, 5), LocalDate.of(2021, 3, 7), "on delivered", orderBooks);
+        Order order11 = new Order(10L, LocalDate.of(2021, 3, 10), LocalDate.of(2021, 3, 12), "on delivered", orderChildrenProduct);
+        Order order12 = new Order(10L, LocalDate.of(2021, 3, 15), LocalDate.of(2021, 3, 17), "on delivered", orderToys);
+        Order order13 = new Order(10L, LocalDate.of(2021, 3, 20), LocalDate.of(2021, 3, 22), "on delivered", orderSundry3);
+        Order order14 = new Order(10L, LocalDate.of(2021, 4, 5), LocalDate.of(2021, 4, 7), "on delivered", orderSundry2);
+        Order order15 = new Order(10L, LocalDate.of(2021, 4, 10), LocalDate.of(2021, 4, 12), "on delivered", orderChildrenProduct);
+        Order order16 = new Order(10L, LocalDate.of(2021, 4, 15), LocalDate.of(2021, 4, 17), "on delivered", orderSundry1);
+        Order order17 = new Order(10L, LocalDate.of(2021, 4, 20), LocalDate.of(2021, 4, 22), "on delivered", orderSundry3);
+        Order order18 = new Order(10L, LocalDate.of(2021, 5, 5), LocalDate.of(2021, 5, 7), "on delivered", orderBooks);
+        Order order19 = new Order(10L, LocalDate.of(2021, 5, 10), LocalDate.of(2021, 5, 12), "on delivered", orderSundry1);
+        Order order20 = new Order(10L, LocalDate.of(2021, 5, 15), LocalDate.of(2021, 5, 17), "on delivered", orderSundry3);
+        Order order21 = new Order(10L, LocalDate.of(2021, 5, 20), LocalDate.of(2021, 5, 22), "on delivered", orderToys);
+        Order order22 = new Order(10L, LocalDate.of(2021, 6, 5), LocalDate.of(2021, 6, 7), "on delivered", orderBooks);
+        Order order23 = new Order(10L, LocalDate.of(2021, 6, 10), LocalDate.of(2021, 6, 12), "on delivered", orderSundry2);
+        Order order24 = new Order(10L, LocalDate.of(2021, 6, 15), LocalDate.of(2021, 6, 17), "on delivered", orderToys);
+        Order order25 = new Order(10L, LocalDate.of(2021, 6, 20), LocalDate.of(2021, 6, 22), "on delivered", orderChildrenProduct);
 
 
         List<Customer> customers = new ArrayList<>(Arrays.asList(
                 new Customer(1L, "William", 1L, Set.of(order1, order2, order3, order4, order5)),
-                new Customer(2L, "James", 1L, Set.of(order10, order5, order3, order4, order6)),
-                new Customer(3L, "Henry", 2L, Set.of(order2, order7, order3, order8, order5)),
-                new Customer(4L, "Tessa", 2L, Set.of(order3, order2, order5, order4, order8)),
-                new Customer(5L, "Dina", 3L, Set.of(order4, order1, order3, order9, order10))
+                new Customer(2L, "James", 1L, Set.of(order6, order7, order8, order9, order10)),
+                new Customer(3L, "Henry", 2L, Set.of(order11, order12, order13, order14, order15)),
+                new Customer(4L, "Tessa", 2L, Set.of(order16, order17, order18, order19, order20)),
+                new Customer(5L, "Dina", 3L, Set.of(order21, order22, order23, order24, order25))
         ));
 
 
-        // Задание 1
+        // Задание 12
         List<Product> task1 = customers.stream()
                 .flatMap(customer -> customer.getOrders().stream())
                 .flatMap(order -> order.getProducts().stream())
@@ -96,6 +111,5 @@ public class Main {
                 .sorted(Comparator.comparing(Product::getPrice))
                 .limit(2)
                 .toList();
-
     }
 }
